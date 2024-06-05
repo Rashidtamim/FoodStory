f8d175a3d6989cbf3fdf3545f701ea662e4030d1
Rashid Tamim <rashidtamim999@gmail.com>
2024-06-06 03:03:59 +0600

    First Commit

---
+++ admin/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FoodStory</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <div class="main_menu">
+        <div class="wrapper">
+        This is a menu section
+        </div>
+    </div>
+
+
+
+    <div class="content">
+    <div class="wrapper">
+        This is a main section
+        </div>
+    </div>
+
+
+
+    <footer class="footer">
+        <div class="wrapper">
+        <p class="text-center">2024 All rights reserved, Some Restaurant. Developed By - <a href="#" style="text-decoration:none">RR Tamim</a></p>
+        </div>
+    </footer>
+</body>
+</html>
